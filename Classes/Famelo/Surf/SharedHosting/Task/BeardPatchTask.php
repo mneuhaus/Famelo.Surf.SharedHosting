a1eced083f609eb76ee811accc83a68c2f85061a
Marc Neuhaus <apocalip@gmail.com>
2013-08-14 14:35:19 +0200

    [FEATURE] Added Tasks for Beard

---
+++ Classes/Famelo/Surf/SharedHosting/Task/BeardPatchTask.php
@@ -0,0 +1,45 @@
+<?php
+namespace Famelo\Surf\SharedHosting\Task;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Surf".            *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Surf\Domain\Model\Node;
+use TYPO3\Surf\Domain\Model\Application;
+use TYPO3\Surf\Domain\Model\Deployment;
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Downloads composer into the current releasePath.
+ */
+class BeardPatchTask extends \TYPO3\Surf\Domain\Model\Task {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Surf\Domain\Service\ShellCommandService
+	 */
+	protected $shell;
+
+	/**
+	 * @param \TYPO3\Surf\Domain\Model\Node $node
+	 * @param \TYPO3\Surf\Domain\Model\Application $application
+	 * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+	 * @param array $options
+	 * @throws \TYPO3\Surf\Exception\TaskExecutionException
+	 * @return void
+	 */
+	public function execute(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		$applicationReleasePath = $deployment->getApplicationReleasePath($application);
+
+
+		$command = $application->getOption('phpPath') . ' beard.phar patch';
+
+		$command = sprintf('cd %s && %s', escapeshellarg($applicationReleasePath), $command);
+		$this->shell->executeOrSimulate($command, $node, $deployment);
+	}
+}
+
+?>
---
+++ Classes/Famelo/Surf/SharedHosting/Task/DownloadBeardTask.php
@@ -0,0 +1,44 @@
+<?php
+namespace Famelo\Surf\SharedHosting\Task;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Surf".            *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Surf\Domain\Model\Node;
+use TYPO3\Surf\Domain\Model\Application;
+use TYPO3\Surf\Domain\Model\Deployment;
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Downloads composer into the current releasePath.
+ */
+class DownloadBeardTask extends \TYPO3\Surf\Domain\Model\Task {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Surf\Domain\Service\ShellCommandService
+	 */
+	protected $shell;
+
+	/**
+	 * @param \TYPO3\Surf\Domain\Model\Node $node
+	 * @param \TYPO3\Surf\Domain\Model\Application $application
+	 * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+	 * @param array $options
+	 * @throws \TYPO3\Surf\Exception\TaskExecutionException
+	 * @return void
+	 */
+	public function execute(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		$applicationReleasePath = $deployment->getApplicationReleasePath($application);
+
+		$command = 'curl -s https://raw.github.com/mneuhaus/Beard-Versions/master/beard-current.phar > beard.phar';
+
+		$command = sprintf('cd %s && %s && chmod +x beard.phar', escapeshellarg($applicationReleasePath), $command);
+		$this->shell->executeOrSimulate($command, $node, $deployment);
+	}
+}
+
+?>
